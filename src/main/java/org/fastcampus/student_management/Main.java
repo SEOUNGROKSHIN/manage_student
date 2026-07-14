cb4f83dd0de35a87ff30b27c965b8a1622c846ee
SHINSEOUNGROK <tnsfhr03@gmail.com>
2026-07-14 17:28:11 +0900

    vo, 일급 컬렉션, 객체지향 생활체조 학습완료

--- src/main/java/org/fastcampus/student_management/Main.java
+++ src/main/java/org/fastcampus/student_management/Main.java
@@ -1,7 +1,9 @@
 package org.fastcampus.student_management;
 
 import org.fastcampus.student_management.application.course.CourseService;
+import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
+import org.fastcampus.student_management.application.student.dto.StudentInfoDto;
 import org.fastcampus.student_management.repo.CourseRepository;
 import org.fastcampus.student_management.repo.StudentRepository;
 import org.fastcampus.student_management.ui.course.CourseController;
@@ -24,6 +26,30 @@ public class Main {
 
     CourseController courseController = new CourseController(coursePresenter, courseService, studentPresenter);
     StudentController studentController = new StudentController(studentPresenter, studentService);
+
+    // 기본 default 세팅 추가
+    StudentInfoDto studentInfoDto = new StudentInfoDto("홍길동", 20, "강남");
+    StudentInfoDto studentInfoDto1 = new StudentInfoDto("김철수", 20, "수서");
+    studentService.saveStudent(studentInfoDto);
+    studentService.saveStudent(studentInfoDto1);
+
+    CourseInfoDto courseInfoDto1 = new CourseInfoDto(
+            "JAVA",
+            1000,
+            "MONDAY",
+            "홍길동",
+            10L
+    );
+
+    CourseInfoDto courseInfoDto2 = new CourseInfoDto(
+            "SPRING",
+            2000,
+            "MONDAY",
+            "김철수",
+            14L
+    );
+    courseService.registerCourse(courseInfoDto1);
+    courseService.registerCourse(courseInfoDto2);
 
     studentPresenter.showMenu();
     UserInputType userInputType = studentController.getUserInput();
--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -2,12 +2,27 @@ package org.fastcampus.student_management.application.course;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
 import org.fastcampus.student_management.domain.Course;
+import org.fastcampus.student_management.domain.CourseList;
 import org.fastcampus.student_management.domain.DayOfWeek;
 import org.fastcampus.student_management.domain.Student;
 import org.fastcampus.student_management.repo.CourseRepository;
+import org.fastcampus.student_management.ui.student.StudentController;
+
+/*1. 일별 수업 정보 반환:
+        - 요일에 해당하는 수업들을 반환해야함
+        - 요일 입력은 MONDAY, TUESDAY 와 같은 식으로 콘솔로 입력 받을 수 있어야 함
+    - 단, 학생 상태가 비활성화 상태이면 수업을 반환해서는 안됨
+2. 수강생들의 상태를 변경 할 수 있음:
+        - 학생이 활성 상태 일 때에는 일별 수업에서 포함이 되어야 함
+    - 학생이 비활성 상태일 때에는, 일별 수업에 포함되서는 안 됨
+    - 활동 상태에서 활동 상태로, 비활성 상태에서 비활성 상태로 변경이 되어서는 안 됨
+3. 수강생들의 수강료를 변경 할 수 있음:
+        - 특정 학생의 수강료를 변경 시키면 특정 학생 수업에 전체에 적용이 되어야 함*/
 
 public class CourseService {
   private final CourseRepository courseRepository;
@@ -26,10 +41,15 @@ public class CourseService {
 
   public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
     // TODO: 과제 구현 부분
-    return new ArrayList<>();
+//    요일에 해당하는 수업들을 반환해야함
+    List<Course> courses = courseRepository.getCourseDayOfWeek(dayOfWeek);
+    return courses.stream().map(CourseInfoDto::new).toList();
   }
 
   public void changeFee(String studentName, int fee) {
     // TODO: 과제 구현 부분
+    List<Course> courses = courseRepository.getCourseListByStudent(studentName);
+    CourseList courseList = new CourseList(courses);
+    courseList.changeAllCoursesFee(fee);
+    }
   }
-}
